b929e6850e102067f24d5a88356b578774eb1420
luisousa <luisousa@gmail.com>
2014-03-22 20:51:25 +0000

    MESSAGE

---
+++ app/models/listing.rb
@@ -0,0 +1,7 @@
+class Listing < ActiveRecord::Base
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+
+		#validates_attachment :image, :presence => true
+
+end
